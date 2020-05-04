407b17c7e0bfc9570b3ccadc595a6eb00b9deca4
Ahsan <ahsan024@gmail.com>
2020-05-04 11:04:59 +0200

    converting App.js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import './Styles.css';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header theme container">
+      <header className="App-header theme container dark-text">
         <div>BMI Healthy Weight Calculator</div>
       </header>
       <Main/>
+
+      <Footer/>
     </div>
   );
 }
