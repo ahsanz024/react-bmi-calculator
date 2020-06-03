3019a5f5269243ea2ac7dd8f7a383a3061e3b3d9
Ahsan <ahsan024@gmail.com>
2020-06-03 18:16:03 +0200

    add: BMI Ranges json file

---
+++ src/components/BmiRanges.js
@@ -0,0 +1,9 @@
+export default {
+  "Under Weight": 18.4,
+  Normal: 22.9,
+  "Over Weight": 24.9,
+  "Pre-Obese": 29.9,
+  Obese: 40,
+  "Obese-2 (morbid obese)": 50,
+  "Obese-2 (super obese)": 50.1,
+};
